d20a5b3646af28e0db7868e15519750dec503ac3
benjamincourthez <b.courthez@gmail.com>
2015-11-11 03:57:56 +0100

    mise à jour de la page shiva

---
+++ shiva.php
@@ -0,0 +1,25 @@
+<?php include("head.php"); ?>
+
+<body>
+
+	<header>
+		<div id="logo2">
+            <img src="images/logo2.png" alt="Logo de Wild Circus 2" id="logo2" />
+        </div>
+                <div id="titre_principal">Wild Circus
+                <img src="images/logo.png" alt="Logo de Wild Circus" id="logo" />
+            </div>
+
+<?php include("menu.php"); ?>
+</header>
+<div id="banniere_shiva">
+	<div id="banniere_shiva_description">
+                    "SHIVA" : The Ice Queen
+                </div>
+            </div>
+            <div id="shiva_description">Shiva the ice queen is a great talented in the field of ice skating.
+She can make countless acrobatics all as spectacular as each other .
+She joined the Wild Circus 5 years ago and she continues to grow each year.
+His show is a glacial beauty !</div>
+</body>
+</html>
