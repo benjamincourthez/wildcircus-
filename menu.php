9ac390b0f60d67bb7ac513fe9167b10383e0d4be
benjamincourthez <b.courthez@gmail.com>
2015-11-11 04:22:55 +0100

    mise à jour du menu

---
+++ menu.php
@@ -0,0 +1,24 @@
+
+<nav ul class="nav">
+	<li>
+		<a href="home.php">Home</a>
+	</li>
+	<li>
+		<a href="home.php">The Show</a>
+		<ul class="dropdown">
+	<li>
+		<a href="shiva.php"><b>"Shiva": The Ice Quenn</b></a>
+			</li>
+			<li>
+				<a href="heloim.php"><b>"Héloim": The Magician</b></a>
+			</li>
+			<li>
+				<a href="Kumaha.php"><b>"Kumaha": The Trainer</b></a>
+				</li>
+	</ul>
+</li>
+<li>
+	<a href="lottery.php">Lottery</a>
+</li>
+<li>
+	<a href="contact.php">Contact</a>
